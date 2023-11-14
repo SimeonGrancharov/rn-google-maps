ec23035a47e8b9503fcc92ccddf8f1f8e3c07405
Simeon Grancharov <skgryncharov@gmail.com>
2023-11-14 16:33:57 +0200

    Styles extraction

--- src/components/PinModalContent.tsx
+++ src/components/PinModalContent.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import { PinT } from '../types/Pin'
 import { Separator } from './Separator'
 import { getConnectorStatusColor } from '../utils/getStatusColor'
@@ -13,33 +13,12 @@ type PropsT = {
 
 export const PinModalContent = (props: PropsT) => {
   return (
-    <View
-      style={{
-        paddingBottom: 20
-      }}
-    >
-      <Text
-        style={{
-          fontSize: 19,
-          fontWeight: '500',
-          textAlign: 'center'
-        }}
-      >
-        {props.pin.title}
-      </Text>
+    <View style={styles.mainContainer}>
+      <Text style={styles.title}>{props.pin.title}</Text>
       <Separator type="vertical" marginTop={15} marginBottom={15} />
 
-      <View
-        style={{
-          flexDirection: 'row'
-        }}
-      >
-        <View
-          style={{
-            flex: 1,
-            alignItems: 'center'
-          }}
-        >
+      <View style={styles.row}>
+        <View style={styles.coordinateContainer}>
           <ItemWithLabel
             value={formatCoordinates('latitude', props.pin.latitude)}
             label="latitude"
@@ -47,12 +26,7 @@ export const PinModalContent = (props: PropsT) => {
         </View>
 
         <Separator type="horizontal" />
-        <View
-          style={{
-            flex: 1,
-            alignItems: 'center'
-          }}
-        >
+        <View style={styles.coordinateContainer}>
           <ItemWithLabel
             value={formatCoordinates('longitude', props.pin.longitude)}
             label="longitude"
@@ -61,40 +35,17 @@ export const PinModalContent = (props: PropsT) => {
       </View>
 
       <View>
-        <Text
-          style={{
-            fontSize: 17,
-            marginTop: 15,
-            fontWeight: '500',
-            lineHeight: 23
-          }}
-        >
-          Connectors
-        </Text>
-        <View
-          style={{
-            marginTop: 10
-          }}
-        >
+        <Text style={styles.titleSmall}>Connectors</Text>
+        <View style={styles.connectorsContainer}>
           {props.pin.connectors.map((connector, idx) => (
-            <View
-              key={idx}
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'flex-start',
-                alignItems: 'center',
-                marginBottom: 10
-              }}
-            >
+            <View key={idx} style={styles.connectorContainer}>
               <Icon
                 size={32}
                 name={getIconNameFromConnectorType(connector.type)}
                 color="#363636"
-                style={{
-                  marginRight: 20
-                }}
+                style={styles.icon}
               />
-              <View style={{ flex: 1 }}>
+              <View style={styles.connectorInfo}>
                 <ItemWithLabel
                   key={idx}
                   value={connector.status}
@@ -111,3 +62,40 @@ export const PinModalContent = (props: PropsT) => {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    paddingBottom: 20
+  },
+  title: {
+    fontSize: 19,
+    fontWeight: '500',
+    textAlign: 'center'
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  coordinateContainer: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  titleSmall: {
+    fontSize: 17,
+    marginTop: 15,
+    fontWeight: '500',
+    lineHeight: 23
+  },
+  connectorsContainer: {
+    marginTop: 10
+  },
+  connectorContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  connectorInfo: { flex: 1 },
+  icon: {
+    marginRight: 20
+  }
+})
